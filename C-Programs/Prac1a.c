2a565685a7521cd7b3131f7ec4f51a48b05996bc
VARDHAMANPATEL23 <vardhamanpatel084@gmail.com>
2023-08-25 19:15:31 +0530

    Prac-1(a) Commited

---
+++ C-Programs/Prac1a.c
@@ -0,0 +1,11 @@
+/* This code is for 1st Practical Part-A */
+#include <stdio.h> //If You used int main remember to use return 0; or else ignore.
+void main()
+{
+    printf("Name: \"xyz\"\n");
+    printf("College: CGPIT");
+    printf("Branch\tName:\tComputer\n");
+    printf("Engineering Subject: Basics of\n");
+    printf("Logic Development Enrolment\n");
+    printf("No.:201204101510005\n");
+}
