6415e786a177cead6d67722c356d1e1640630c6e
DESTRUCTIVE-GAMER <67853024+VARDHAMANPATEL23@users.noreply.github.com>
2023-08-27 21:59:44 +0530

    Update Prac5c.c

---
+++ C-Programs/Prac5c.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main() {
+    int choice, rows;
+
+    printf("Choose a pattern to print:\n");
+    printf("1. Right Triangle\n");
+    printf("2. Pyramid\n");
+    printf("3. Right Triangle of Numbers\n");
+    printf("4. Upside-down Right Triangle of Alphabets\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1: // Right Triangle
+            printf("Enter the number of rows: ");
+            scanf("%d", &rows);
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 1; j <= i; j++) {
+                    printf("* ");
+                }
+                printf("\n");
+            }
+            break;
+
+        case 2: // Pyramid
+            printf("Enter the number of rows: ");
+            scanf("%d", &rows);
+            for (int i = 1; i <= rows; i++) {
+                for (int space = 1; space <= rows - i; space++) {
+                    printf(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    printf("*");
+                }
+                printf("\n");
+            }
+            break;
+
+        case 3: // Right Triangle of Numbers
+            printf("Enter the number of rows: ");
+            scanf("%d", &rows);
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 1; j <= i; j++) {
+                    printf("%d ", j);
+                }
+                printf("\n");
+            }
+            break;
+
+        case 4: // Upside-down Right Triangle of Alphabets
+            printf("Enter the number of rows: ");
+            scanf("%d", &rows);
+            for (int i = rows; i >= 1; i--) {
+                char ch = 'A';
+                for (int j = 1; j <= i; j++) {
+                    printf("%c ", ch);
+                    ch++;
+                }
+                printf("\n");
+            }
+            break;
+
+        default:
+            printf("Invalid choice.\n");
+    }
+
+    return 0;
+}
