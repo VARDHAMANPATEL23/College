27f6fd9597cf834a844abb42241d99cc2813726d
VARDHAMANPATEL23 <vardhamanpatel084@gmail.com>
2023-08-31 06:30:24 +0530

    Changes Prac 4a commit

--- C-Programs/Prac4a.c
+++ C-Programs/Prac4a.c
@@ -5,24 +5,24 @@ void main()
     printf("Enter marks from 1 to 100 : ");
     scanf("%d", &marks);
 
-    if (marks <= 100 || marks >= 80)
+    if (marks <= 100 && marks >= 80)
     {
         printf("Distinction.\n");
     }
-    else if (marks <= 79 || marks >= 60)
+    else if (marks <= 79 && marks >= 60)
     {
         printf("First Class.\n");
     }
-    else if (marks <= 59 || marks >= 40)
+    else if (marks <= 59 && marks >= 40)
     {
         printf("Second Class.\n");
     }
-    else if (marks < 40)
-    {
-        printf("Fail.\n");
-    }
-    else
+    else if (marks > 100 && marks < 0)
     {
         printf("Wrong input marks.\n");
+    }
+    else if (marks < 40 && marks >= 0)
+    {
+        printf("Fail.\n");
     }
 }
